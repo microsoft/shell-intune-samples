cdc87991e10352d40f6451c64f93c8eee106f9e6
Arnab Biswas [MSFT] <arnab@microsoft.com>
2020-07-01 23:45:19 -0400

    Convert DMG to INTUNEMAC

--- LOBAppPrep/DmgtoIntunemac.sh
+++ LOBAppPrep/DmgtoIntunemac.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+# From a terminal window, run this dmg conversion script, including the input arguments as required below. 
+# The output file will be written to the location the script is ran from.
 # Input flags + arguments
 #   -d: (required) dmg path
 #   -n: (required) desired output filename (no extension)
 #   -s: (optional) DeveloperID to sign pkg (common cert)
 #   -p: (optional) Output a .pkg rather than .intunemac
-# The output file will be written to the location the script is ran from.
 #
 # Example:
 # ./DmgtoIntunemac.sh -d ~/Downloads/Spotify.dmg -n SpotifyFinal
 #
 # Notes:
-# 1. Make sure that the DmgtoIntunemac.sh shell script is located in the same directory as the Intune App Wrapping Tool (IntuneAppUtil). The name of this directory when downloaded from Microsoft is: intune-app-wrapping-tool-mac-master. 
+# 1. Make sure that the DmgtoIntunemac.sh shell script is located in the same directory as the Intune App Wrapping Tool (IntuneAppUtil) found at https://github.com/msintuneappsdk/intune-app-wrapping-tool-mac. The name of this directory when downloaded from Microsoft is: intune-app-wrapping-tool-mac-master. 
 # 2. Although the signing flag is optional for this conversion script, packages must be signed in order to successfully install as a managed app. 
 
 
