84dc2dc9480bf259ca7082ea4145d268852d9fe6
Neil Johnson <neiljohn@microsoft.com>
2020-07-16 12:52:59 +0100

    Initial example scripts
    As requested some example scripts for the scripting agent.

---
+++ Apps/gitHub Desktop/installGitHubDesktop.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#set -x
+
+############################################################################################
+##
+## Script to download latest gitHub Desktop for macOS
+##
+###########################################
+
+## Copyright (c) 2020 Microsoft Corp. All rights reserved.
+## Scripts are not supported under any Microsoft standard support program or service. The scripts are provided AS IS without warranty of any kind.
+## Microsoft disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a
+## particular purpose. The entire risk arising out of the use or performance of the scripts and documentation remains with you. In no event shall
+## Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever
+## (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary
+## loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility
+## of such damages.
+## Feedback: neiljohn@microsoft.com
+
+# Define variables
+
+tempdir="/tmp"
+tempfile="/tmp/GitHubDesktop.zip"
+weburl="https://central.github.com/deployments/desktop/desktop/latest/darwin"
+appname="GitHub Desktop"
+log="/var/log/installgithubdesktop.log"
+
+# start logging
+
+exec 1>> $log 2>&1
+
+# Begin Script Body
+
+echo ""
+echo "##############################################################"
+echo "# $(date) | Starting install of $appname"
+echo "############################################################"
+echo ""
+
+# Let's download the files we need and attempt to install...
+
+echo "$(date) | Downloading $appname"
+curl -L -f -o $tempfile $weburl
+if [ "$?" = "0" ]; then
+   echo "$(date) | $appname downloaded to $tempfile"
+else
+   echo "$(date) | Failed to download from $weburl"
+   exit 1
+fi
+
+echo "$(date) | Installing $appname"
+cd $tempdir
+unzip $tempfile
+if [ "$?" = "0" ]; then
+   echo "$(date) | $tmpfile unzipped"
+else
+   echo "$(date) | failed to unzip $tmpfile"
+   exit 2
+fi
+
+mv -f "GitHub Desktop.app" /Applications
+if [ "$?" = "0" ]; then
+   echo "$(date) | $appname moved into /Applications"
+else
+   echo "$(date) | failed to move $appname to /Applications"
+   exit 3
+fi
+
+echo "$(date) | Fix up permissions"
+sudo chown -R root:wheel /Applications/VOX.app
+if [ "$?" = "0" ]; then
+   echo "$(date) | correctly applied permissions to $appname"
+else
+   echo "$(date) | failed to apply permissions to $appname"
+   exit 4
+fi
---
+++ Apps/gitHub Desktop/readme.md
@@ -0,0 +1,15 @@
+# gitHub  Installation Script
+
+This script is am example showing how to use the [Intune Shell Scripting(https://docs.microsoft.com/en-us/mem/intune/apps/macos-shell-scripts)] feature to install an application. In this instance the script downloads the gitHub Desktop files, uncompresses them and installs into the /Applications directory.
+
+## Scenario
+
+This scripts intended usage scenario is to deploy the gitHub Desktop app to Mac devices that need to use it.
+
+
+## Script Settings
+
+Run script as signed-in user : No
+Hide script notifications on devices : Not configured
+Script frequency : Not configured
+Mac number of times to retry if script fails : 3
