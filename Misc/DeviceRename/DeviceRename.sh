571376ac40d91ba8a3675c656087f458d650dd20
paolomata <74918781+paolomata@users.noreply.github.com>
2021-01-25 11:24:16 -0500

    submitting a macOS device rename script based on device type and serial # (#11)
    * Create readme.md
    
    * Add files via upload
    
    * Update rename-mac.sh
    
    * Update rename-mac.sh
    
    * Update rename-mac.sh
    
    removed designation portion to avoid asking the user for a prefix
    
    * Update readme.md
    
    * Update readme.md
    
    * Update readme.md
    
    * Update rename-mac.sh
    
    * Update and rename rename-mac.sh to DeviceRename.sh
    
    * Update readme.md
    
    * Update readme.md
    
    * Rename Misc/renameMac/readme.md to Misc/DeviceRename/readme.md
    
    * Rename Misc/renameMac/DeviceRename.sh to Misc/DeviceRename/DeviceRename.sh
    
    * Device rename
    
    Co-authored-by: Neil Johnson <neiljohn@microsoft.com>

---
+++ Misc/DeviceRename/DeviceRename.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+#set -x
+
+############################################################################################
+##
+## Script to rename a Mac device based on device type and serial number
+##
+############################################################################################
+
+## Copyright (c) 2021 Microsoft Corp. All rights reserved.
+## Scripts are not supported under any Microsoft standard support program or service. The scripts are provided AS IS without warranty of any kind.
+## Microsoft disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a
+## particular purpose. The entire risk arising out of the use or performance of the scripts and documentation remains with you. In no event shall
+## Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever
+## (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary
+## loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility
+## of such damages.
+## Feedback: paoloma@microsoft.com
+
+## Define variables
+appname="DeviceRename"
+logandmetadir="/Library/Logs/Microsoft/Intune/Scripts/$appname"
+log="$logandmetadir/$appname.log"
+
+## Check if the log directory has been created
+if [ -d $logandmetadir ]; then
+    ## Already created
+    echo "# $(date) | Log directory already exists - $logandmetadir"
+else
+    ## Creating Metadirectory
+    echo "# $(date) | creating log directory - $logandmetadir"
+    mkdir -p $logandmetadir
+fi
+
+# start logging
+exec 1>> $log 2>&1
+
+# Begin Script Body
+echo ""
+echo "##############################################################"
+echo "# $(date) | Starting $appname"
+echo "############################################################"
+echo ""
+
+echo " $(date) | Checking if renaming is necessary"
+
+SerialNum=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}' | cut -d ':' -f2- | xargs)
+if [ "$?" = "0" ]; then
+  echo " $(date) | Serial detected as $SerialNum"
+else
+   echo "$(date) | Unable to determine serial number"
+   exit 1
+fi
+
+
+CurrentNameCheck=$(scutil --get ComputerName)
+if [ "$?" = "0" ]; then
+  echo " $(date) | Current computername detected as $CurrentNameCheck"
+else
+   echo "$(date) | Unable to determine current name"
+   exit 1
+fi
+
+
+
+if [[ "$CurrentNameCheck" == *"$SerialNum"* ]]
+  then
+  echo "# $(date) | rename not necessary as the Mac name already includes the serial number. Exiting..."
+  exit 0
+fi
+
+echo " $(date) | Old Name: $CurrentNameCheck"
+ModelName=$(system_profiler SPHardwareDataType | awk /'Model Name: '/ | cut -d ':' -f2- | xargs)
+if [ "$?" = "0" ]; then
+  echo " $(date) | Retrieved model name: $ModelName"
+else
+   echo "$(date) | Unable to determine modelname"
+   exit 1
+fi
+
+
+echo " $(date) | Generating four characters code based on retrieved model name $ModelName"
+
+case $ModelName in
+  MacBook\ Air*) ModelCode=MABA;;
+  MacBook\ Pro*) ModelCode=MABP;;
+  MacBook*) ModelCode=MACB;;
+  iMac*) ModelCode=IMAC;;
+  Mac\ Pro*) ModelCode=MACP;;
+  Mac\ mini*) ModelCode=MINI;;
+  *) ModelCode=$(echo $ModelName | tr -d ' ' | cut -c1-4);;
+esac
+
+echo " $(date) | ModelCode variable set to $ModelCode"
+echo " $(date) | Retrieved serial number: $SerialNum"
+echo " $(date) | Building the new name..."
+NewName=$ModelCode$SerialNum
+
+echo " $(date) | Generated Name: $NewName"
+
+#Setting ComputerName
+scutil --set ComputerName $NewName
+if [ "$?" = "0" ]; then
+   echo " $(date) | Computername changed from $CurrentNameCheck to $NewName"
+else
+   echo " $(date) | Failed to rename the device from $CurrentNameCheck to $NewName"
+   exit 1
+fi
+
+#Setting HostName
+scutil --set HostName $NewName
+if [ "$?" = "0" ]; then
+   echo " $(date) | HostName changed from $CurrentNameCheck to $NewName"
+else
+   echo " $(date) | Failed to rename the device from $CurrentNameCheck to $NewName"
+   exit 1
+fi
+
+#Setting LocalHostName
+scutil --set LocalHostName $NewName
+if [ "$?" = "0" ]; then
+   echo " $(date) | LocalHostName changed from $CurrentNameCheck to $NewName"
+else
+   echo " $(date) | Failed to rename the device from $CurrentNameCheck to $NewName"
+   exit 1
+fi
---
+++ Misc/DeviceRename/readme.md
@@ -0,0 +1,53 @@
+# Script to rename a Mac device based on model type and serial number
+
+This script renames a Mac device by looking at the model type and at the serial number
+This is ideal for devices that are enrolled without user affinity. The script can be further customized to include the user name as part of the device rename.
+
+## DeviceRename.sh
+
+The script consists of three steps:
+1) determine the model type and, based on the retrieved type, set a 4 characters variable $ModelCode
+    e.g. MacBook Air ==> $ModelCode = MABA
+2) collect the serial number and keep the first 10 characters
+    e.g. Serial Number = C02BA222DC79 ==> $SerialNum = C02BA222DC
+3) build the final name by combining $ModelCode and $serial
+    e.g. $NewName = MABAC02BA222DC
+
+```
+# Define variables
+appname="DeviceRename"
+logandmetadir="/Library/Logs/Microsoft/Intune/Scripts/$appname"
+log="$logandmetadir/$appname.log"
+```
+
+## Script Settings
+
+- Run script as signed-in user : No
+- Hide script notifications on devices : Not configured
+- Script frequency : Not configured
+- Mac number of times to retry if script fails : 3
+
+### Log file example
+
+>**Note:** The log file will output to **/Library/Logs/Microsoft/Intune/Scripts/DeviceRename/DeviceRename.log** by default. Exit status is either 0 or 1. To gather this log with Intune remotely take a look at  [Troubleshoot macOS shell script policies using log collection](https://docs.microsoft.com/en-us/mem/intune/apps/macos-shell-scripts#troubleshoot-macos-shell-script-policies-using-log-collection)
+
+```
+##############################################################
+# Mon Jan 25 16:18:28 GMT 2021 | Starting DeviceRename
+############################################################
+
+ Mon Jan 25 16:18:28 GMT 2021 | Checking if renaming is necessary
+ Mon Jan 25 16:18:29 GMT 2021 | Serial detected as ABCDEF000017
+ Mon Jan 25 16:18:29 GMT 2021 | Current computername detected as TestVM
+ Mon Jan 25 16:18:29 GMT 2021 | Old Name: TestVM
+ Mon Jan 25 16:18:30 GMT 2021 | Retrieved model name: MacBook Pro
+ Mon Jan 25 16:18:30 GMT 2021 | Generating four characters code based on retrieved model name MacBook Pro
+ Mon Jan 25 16:18:30 GMT 2021 | ModelCode variable set to MABP
+ Mon Jan 25 16:18:30 GMT 2021 | Retrieved serial number: ABCDEF000017
+ Mon Jan 25 16:18:30 GMT 2021 | Building the new name...
+ Mon Jan 25 16:18:30 GMT 2021 | Generated Name: MABPABCDEF000017
+ Mon Jan 25 16:18:30 GMT 2021 | Computername changed from TestVM to MABPABCDEF000017
+ Mon Jan 25 16:18:30 GMT 2021 | HostName changed from TestVM to MABPABCDEF000017
+ Mon Jan 25 16:18:30 GMT 2021 | LocalHostName changed from TestVM to MABPABCDEF000017
+
+```
