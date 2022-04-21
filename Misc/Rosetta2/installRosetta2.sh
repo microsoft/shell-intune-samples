0830bf9a163da0db2753a397ef3c0e7934933c87
Neil Johnson <neiljohn@microsoft.com>
2022-04-21 17:21:26 +0100

    Bug fix
    An old version got checked in by mistake. Fixed some issues with waitForProcess and incorrect CLI for softwareupdate

--- Misc/Rosetta2/installRosetta2.sh
+++ Misc/Rosetta2/installRosetta2.sh
@@ -51,6 +51,55 @@ isSoftwareUpdateRunning () {
 
 }
 
+# function to delay script if the specified process is running
+waitForProcess () {
+
+    #################################################################################################################
+    #################################################################################################################
+    ##
+    ##  Function to pause while a specified process is running
+    ##
+    ##  Functions used
+    ##
+    ##      None
+    ##
+    ##  Variables used
+    ##
+    ##      $1 = name of process to check for
+    ##      $2 = length of delay (if missing, function to generate random delay between 10 and 60s)
+    ##      $3 = true/false if = "true" terminate process, if "false" wait for it to close
+    ##
+    ###############################################################
+    ###############################################################
+
+    processName=$1
+    fixedDelay=$2
+    terminate=$3
+
+    echo "$(date) | Waiting for other [$processName] processes to end"
+    while ps aux | grep "$processName" | grep -v grep &>/dev/null; do
+
+        if [[ $terminate == "true" ]]; then
+            echo "$(date) | + [$appname] running, terminating [$processpath]..."
+            pkill -f "$processName"
+            return
+        fi
+
+        # If we've been passed a delay we should use it, otherwise we'll create a random delay each run
+        if [[ ! $fixedDelay ]]; then
+            delay=$(( $RANDOM % 50 + 10 ))
+        else
+            delay=$fixedDelay
+        fi
+
+        echo "$(date) |  + Another instance of $processName is running, waiting [$delay] seconds"
+        sleep $delay
+    done
+    
+    echo "$(date) | No instances of [$processName] found, safe to proceed"
+
+}
+
 # function to check if we need Rosetta 2
 checkForRosetta2 () {
 
@@ -99,7 +148,7 @@ checkForRosetta2 () {
             if /usr/bin/pgrep oahd >/dev/null 2>&1; then
                 echo "$(date) | Rosetta is already installed and running. Nothing to do."
             else
-                /usr/sbin/softwareupdate –install-rosetta –agree-to-license
+                /usr/sbin/softwareupdate --install-rosetta --agree-to-license
             
                 if [[ $? -eq 0 ]]; then
                     echo "$(date) | Rosetta has been successfully installed."
