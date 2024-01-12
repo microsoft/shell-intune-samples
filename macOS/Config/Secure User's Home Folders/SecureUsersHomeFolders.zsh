02d798a0e2603f48c9ab56470bb67ea854dd022f
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2024-01-12 09:58:27 +0200

    Access to Secure User's Home Folders
    This Custom Script is required when implementing following CIS or NIST Recommendations for macOS: 
    - CIS: Ensure Home Folders Are Secure (Automated)
    - NIST: Secure User's Home Folders

---
+++ macOS/Config/Secure User's Home Folders/SecureUsersHomeFolders.zsh
@@ -0,0 +1,55 @@
+#!/bin/zsh
+#set -x
+############################################################################################
+##
+## Script to secure user's home folders
+##
+############################################################################################
+
+## Copyright (c) 2023 Microsoft Corp. All rights reserved.
+## Scripts are not supported under any Microsoft standard support program or service. The scripts are provided AS IS without warranty of any kind.
+## Microsoft disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a
+## particular purpose. The entire risk arising out of the use or performance of the scripts and documentation remains with you. In no event shall
+## Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever
+## (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary
+## loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility
+## of such damages.
+## Feedback: neiljohn@microsoft.com
+
+# Define variables
+appname="SecureUsersHomeFolders"
+logandmetadir="/Library/Logs/Microsoft/IntuneScripts/$appname"
+log="$logandmetadir/$appname.log"
+
+# Check if the log directory has been created
+if [ -d $logandmetadir ]; then
+    # Already created
+    echo "$(date) | Log directory already exists - $logandmetadir"
+else
+    # Creating Metadirectory
+    echo "$(date) | creating log directory - $logandmetadir"
+    mkdir -p $logandmetadir
+fi
+
+# Secure User's Home Folders
+SecureUsersHomeFolders() {
+  IFS=$'\n'
+  for userDirs in $( /usr/bin/find /System/Volumes/Data/Users -mindepth 1 -maxdepth 1 -type d ! \( -perm 700 -o -perm 711 \) | /usr/bin/grep -v "Shared" | /usr/bin/grep -v "Guest" ); do
+    /bin/chmod og-rwx "$userDirs"
+  done
+  unset IFS
+echo  "$(date) | User's Home Folders are now secured or already secured. Closing script..."
+}
+
+# Start logging
+exec &> >(tee -a "$log")
+
+# Begin Script Body
+echo ""
+echo "##############################################################"
+echo "# $(date) | Starting running of script $appname"
+echo "############################################################"
+echo ""
+
+# Run function
+SecureUsersHomeFolders
