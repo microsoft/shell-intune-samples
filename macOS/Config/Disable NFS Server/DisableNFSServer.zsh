10a1ca78cd48e62aef64c7b076c09b1171bbc356
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2024-01-12 09:39:35 +0200

    Disable NFS Server
    This Custom Script is required when implementing following CIS or NIST Recommendations for macOS:
    - CIS: Ensure NFS Server Is Disabled (Automated)
    - NIST: Disable Network File System Service

---
+++ macOS/Config/Disable NFS Server/DisableNFSServer.zsh
@@ -0,0 +1,55 @@
+#!/bin/zsh
+#set -x
+############################################################################################
+##
+## Script to disable NFS Server
+##
+############################################################################################
+
+## Copyright (c) 2023 Microsoft Corp. All rights reserved.
+## Scripts are not supported under any Microsoft standard support program or service. The scripts are provided AS IS without warranty of any kind.
+## Microsoft disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a
+## particular purpose. The entire risk arising out of the use or performance of the scripts and documentation remains with you. In no event shall
+## Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever
+## (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary
+## loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility
+## of such damages.
+## Feedback: neiljohn@microsoft.com
+
+# Define variables
+appname="DisableNFSServer"
+logandmetadir="/Library/Logs/Microsoft/IntuneScripts/$appname"
+log="$logandmetadir/$appname.log"
+
+# Check if the log directory has been created
+if [ -d $logandmetadir ]; then
+    # Already created
+    echo "$(date) | Log directory already exists - $logandmetadir"
+else
+    # Creating Metadirectory
+    echo "$(date) | creating log directory - $logandmetadir"
+    mkdir -p $logandmetadir
+fi
+
+# Disable NFS Server
+DisableNFSServer() {
+/sbin/nfsd stop 2> /dev/null
+/bin/launchctl disable system/com.apple.nfsd 2> /dev/null
+
+# Removing the exported Directory listing
+/bin/rm /etc/exports 2> /dev/null
+echo  "$(date) | NFS Server is disabled or already disabled. Closing script..."
+}
+
+# Start logging
+exec &> >(tee -a "$log")
+
+# Begin Script Body
+echo ""
+echo "##############################################################"
+echo "# $(date) | Starting running of script $appname"
+echo "############################################################"
+echo ""
+
+# Run function
+DisableNFSServer
