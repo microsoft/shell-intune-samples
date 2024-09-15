4db9863d3cb115d1424cecf52bdcd8d04f16e0f1
Jan Parttimaa <jan.parttimaa@kpmg.fi>
2024-09-15 15:29:00 +0300

    Fixed some sentences from the script and readme -file.

--- macOS/Config/Disable creation of '.DS_Store' -files on network shares and removable drives/DisableCreationOfDS_StoreFilesOnNetworkSharesAndRemovableDrives.zsh
+++ macOS/Config/Disable creation of '.DS_Store' -files on network shares and removable drives/DisableCreationOfDS_StoreFilesOnNetworkSharesAndRemovableDrives.zsh
@@ -39,7 +39,7 @@ echo "$(date) | Checking if creation of '.DS_Store' -files have been disabled on
 if [[ "$statusnetworkshares" == "0" ]]; then
     echo  "$(date) | Creation of '.DS_Store' -files have been enabled on network shares for user $USER. Disabling it..."
     defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
-    echo  "$(date) | Creation of '.DS_Store' -files have been disabled on network shares for user $USER. Let's continue..."
+    echo  "$(date) | Creation of '.DS_Store' -files have been disabled on network shares for user $USER. Any changes made will take effect the next time you log in to your Mac-device. Let's continue..."
 else
     echo "$(date) | Creation of '.DS_Store' -files have been already disabled on network shares for user $USER. Let's continue..."
 fi
@@ -54,7 +54,7 @@ if [[ "$statusremovabledrives" == "0" ]]; then
     echo  "$(date) | Creation of '.DS_Store' -files have been disabled on removable drives for user $USER. All done! Any changes made will take effect the next time you log in to your Mac-device. Closing script..."
     exit 0
 else
-    echo "$(date) | Creation of '.DS_Store' -files have been already disabled on removable drives for user $USER. All done! Any changes made will take effect the next time you log in to your Mac-device. Closing script..."
+    echo "$(date) | Creation of '.DS_Store' -files have been already disabled on removable drives for user $USER. All done! Closing script..."
     exit 0
 fi
 }
