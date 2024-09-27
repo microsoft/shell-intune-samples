68db50f1e235121f678412a14e4e3016175923ef
Neil Johnson <neiljohn@microsoft.com>
2024-09-27 16:06:44 +0100

    Updated to support intune package receipt detection
    Removed swiftDialog binary and instead added detection to install into the pre-install script
    Updated sample to handle name change of Remote Desktop to Windows App.

--- macOS/Config/Swift Dialog (PKG)/IntuneScripts/intunePre.sh
+++ macOS/Config/Swift Dialog (PKG)/IntuneScripts/intunePre.sh
@@ -10,6 +10,9 @@
 
 # Define any variables we need here:
 logDir="/Library/Application Support/Microsoft/IntuneScripts/Swift Dialog"
+DIALOG_BIN="/path/to/SwiftDialog"  # Set this to the path where SwiftDialog is expected to be installed
+PKG_PATH="/var/tmp/dialog.pkg"
+PKG_URL="https://github.com/swiftDialog/swiftDialog/releases/download/v2.5.2/dialog-2.5.2-4777.pkg"
 
 # Start Logging
 mkdir -p "$logDir"
@@ -21,6 +24,20 @@ if [ -e "/Library/Application Support/Dialog" ]; then
     rm -rf "/Library/Application Support/SwiftDialogResources"
     rm -rf "/usr/local/bin/dialog"
 fi
+
+# Download the SwiftDialog .pkg
+curl -L -o "$PKG_PATH" "$PKG_URL"
+
+# Install SwiftDialog from the downloaded .pkg file
+sudo installer -pkg "$PKG_PATH" -target /
+  
+if [[ $? -eq 0 ]]; then
+    echo "$(date) | POST | Swift Dialog has been installed successfully."
+else
+    echo "$(date) | ERROR | Swift Dialog installation failed."
+    exit 1
+fi
+
 
 echo "$(date) | PRE | Completed Pre-install script"
 exit 0
