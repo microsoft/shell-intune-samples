a8e04015b05c4214ec92b05ee57caeb270937254
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2025-02-10 12:26:11 +0200

    Add files via upload

--- macOS/Config/Firewall/FirewallBlockPortNumbers.zsh
+++ macOS/Config/Firewall/FirewallBlockPortNumbers.zsh
@@ -26,12 +26,12 @@ port135_udp=true                                                                
 port137_139_tcp=true                                                                                        # Blocks Port Numbers 137-139 TCP used by NetBIOS, which can be a vector for various attacks.
 port137_139_udp=true                                                                                        # Blocks Port Numbers 137-139 UDP used by NetBIOS, which can be a vector for various attacks.
 port445_tcp=true                                                                                            # Blocks Port Number 445 TCP used by Microsoft-DS (Active Directory, Windows shares), which is often targeted by malware.
-port1433-1434_tcp=true                                                                                      # Blocks Port Numbers 1433-1434 TCP used by Microsoft SQL Server, which can be exploited if not properly secured.
-port1433-1434_udp=true                                                                                      # Blocks Port Numbers 1433-1434 UDP used by Microsoft SQL Server, which can be exploited if not properly secured.
+port1433_1434_tcp=true                                                                                      # Blocks Port Numbers 1433-1434 TCP used by Microsoft SQL Server, which can be exploited if not properly secured.
+port1433_1434_udp=true                                                                                      # Blocks Port Numbers 1433-1434 UDP used by Microsoft SQL Server, which can be exploited if not properly secured.
 port3389_tcp=true                                                                                           # Blocks Port Number 3389 TCP used by Remote Desktop Protocol (RDP), which is common target for brute force attacks.
 port1900_udp=true                                                                                           # Blocks Port Number 1900 UDP used by Universal Plud and Play (UPnP), which can be exploited for network discovery and attacks.
-port20-21_tcp=true                                                                                          # Blocks Port Numbers 20-21 TCP used by FTP, which can be insecure if not properly configured.
-port20-21_udp=true                                                                                          # Blocks Port Numbers 20-21 UDP used by FTP, which can be insecure if not properly configured.
+port20_21_tcp=true                                                                                          # Blocks Port Numbers 20-21 TCP used by FTP, which can be insecure if not properly configured.
+port20_21_udp=true                                                                                          # Blocks Port Numbers 20-21 UDP used by FTP, which can be insecure if not properly configured.
 port23_tcp=true                                                                                             # Blocks Port Numbers 23 TCP used by Telnet, which transmits data in plaintext and is insecure.
 
 # Check if the log directory has been created
@@ -145,6 +145,194 @@ port137_139_udp() {
     echo "$(date) | Ports ${PORTS[*]}/$PROTO have been disabled permanently in pf.conf."
 }
 
+# Function for blocking Port Number 445 TCP
+port445_tcp() {
+    PORT=445
+    PROTO="tcp"
+    RULE="block in proto $PROTO from any to any port $PORT"
+
+    # Check if the rule already exists in /etc/pf.conf
+    if grep -q "$RULE" /etc/pf.conf; then
+        echo "$(date) | Port $PORT/$PROTO is already disabled."
+    else
+        echo "$(date) | Disabling port $PORT/$PROTO permanently..."
+
+        # Append the rule to /etc/pf.conf
+        echo "$RULE" | tee -a /etc/pf.conf > /dev/null
+
+        # Reload pf rules
+        pfctl -f /etc/pf.conf >/dev/null 2>&1
+        pfctl -E >/dev/null 2>&1
+
+        echo "$(date) | Port $PORT/$PROTO has been disabled permanently."
+    fi
+}
+
+# Function for blocking Port Numbers 1433-1434 TCP
+port1433_1434_tcp() {
+    PORTS=(1433 1434)
+    PROTO="tcp"
+    PF_CONF="/etc/pf.conf"
+    
+    # Check if the ports are already disabled
+    for PORT in "${PORTS[@]}"; do
+        RULE="block in proto $PROTO from any to any port $PORT"
+        if grep -q "$RULE" $PF_CONF; then
+            echo "$(date) | Port $PORT/$PROTO is already disabled in pf.conf."
+        else
+            echo "$(date) | Disabling port $PORT/$PROTO in pf.conf..."
+            echo "$RULE" | sudo tee -a $PF_CONF > /dev/null
+        fi
+    done
+
+    # Reload pf rules
+    pfctl -f $PF_CONF >/dev/null 2>&1
+    pfctl -E >/dev/null 2>&1
+
+    echo "$(date) | Ports ${PORTS[*]}/$PROTO have been disabled permanently in pf.conf."
+}
+
+# Function for blocking Port Numbers 1433-1434 UDP
+port1433_1434_udp() {
+    PORTS=(1433 1434)
+    PROTO="udp"
+    PF_CONF="/etc/pf.conf"
+    
+    # Check if the ports are already disabled
+    for PORT in "${PORTS[@]}"; do
+        RULE="block in proto $PROTO from any to any port $PORT"
+        if grep -q "$RULE" $PF_CONF; then
+            echo "$(date) | Port $PORT1/$PROTO is already disabled in pf.conf."
+        else
+            echo "$(date) | Disabling port $PORT/$PROTO in pf.conf..."
+            echo "$RULE" | sudo tee -a $PF_CONF > /dev/null
+        fi
+    done
+
+    # Reload pf rules
+    pfctl -f $PF_CONF >/dev/null 2>&1
+    pfctl -E >/dev/null 2>&1
+
+    echo "$(date) | Ports ${PORTS[*]}/$PROTO have been disabled permanently in pf.conf."
+}
+
+# Function for blocking Port Number 3389 TCP
+port3389_tcp() {
+    PORT=3389
+    PROTO="tcp"
+    RULE="block in proto $PROTO from any to any port $PORT"
+
+    # Check if the rule already exists in /etc/pf.conf
+    if grep -q "$RULE" /etc/pf.conf; then
+        echo "$(date) | Port $PORT/$PROTO is already disabled."
+    else
+        echo "$(date) | Disabling port $PORT/$PROTO permanently..."
+
+        # Append the rule to /etc/pf.conf
+        echo "$RULE" | tee -a /etc/pf.conf > /dev/null
+
+        # Reload pf rules
+        pfctl -f /etc/pf.conf >/dev/null 2>&1
+        pfctl -E >/dev/null 2>&1
+
+        echo "$(date) | Port $PORT/$PROTO has been disabled permanently."
+    fi
+}
+
+# Function for blocking Port Number 1900 UDP
+port1900_udp() {
+    PORT=1900
+    PROTO="udp"
+    RULE="block in proto $PROTO from any to any port $PORT"
+
+    # Check if the rule already exists in /etc/pf.conf
+    if grep -q "$RULE" /etc/pf.conf; then
+        echo "$(date) | Port $PORT/$PROTO is already disabled."
+    else
+        echo "$(date) | Disabling port $PORT/$PROTO permanently..."
+
+        # Append the rule to /etc/pf.conf
+        echo "$RULE" | tee -a /etc/pf.conf > /dev/null
+
+        # Reload pf rules
+        pfctl -f /etc/pf.conf >/dev/null 2>&1
+        pfctl -E >/dev/null 2>&1
+
+        echo "$(date) | Port $PORT/$PROTO has been disabled permanently."
+    fi
+}
+
+# Function for blocking Port Numbers 20-21 TCP
+port20_21_tcp() {
+    PORTS=(20 21)
+    PROTO="tcp"
+    PF_CONF="/etc/pf.conf"
+    
+    # Check if the ports are already disabled
+    for PORT in "${PORTS[@]}"; do
+        RULE="block in proto $PROTO from any to any port $PORT"
+        if grep -q "$RULE" $PF_CONF; then
+            echo "$(date) | Port $PORT/$PROTO is already disabled in pf.conf."
+        else
+            echo "$(date) | Disabling port $PORT/$PROTO in pf.conf..."
+            echo "$RULE" | sudo tee -a $PF_CONF > /dev/null
+        fi
+    done
+
+    # Reload pf rules
+    pfctl -f $PF_CONF >/dev/null 2>&1
+    pfctl -E >/dev/null 2>&1
+
+    echo "$(date) | Ports ${PORTS[*]}/$PROTO have been disabled permanently in pf.conf."
+}
+
+# Function for blocking Port Numbers 20-21 UDP
+port20_21_udp() {
+    PORTS=(20 21)
+    PROTO="udp"
+    PF_CONF="/etc/pf.conf"
+    
+    # Check if the ports are already disabled
+    for PORT in "${PORTS[@]}"; do
+        RULE="block in proto $PROTO from any to any port $PORT"
+        if grep -q "$RULE" $PF_CONF; then
+            echo "$(date) | Port $PORT1/$PROTO is already disabled in pf.conf."
+        else
+            echo "$(date) | Disabling port $PORT/$PROTO in pf.conf..."
+            echo "$RULE" | sudo tee -a $PF_CONF > /dev/null
+        fi
+    done
+
+    # Reload pf rules
+    pfctl -f $PF_CONF >/dev/null 2>&1
+    pfctl -E >/dev/null 2>&1
+
+    echo "$(date) | Ports ${PORTS[*]}/$PROTO have been disabled permanently in pf.conf."
+}
+
+# Function for blocking Port Number 23 TCP
+port23_tcp() {
+    PORT=23
+    PROTO="tcp"
+    RULE="block in proto $PROTO from any to any port $PORT"
+
+    # Check if the rule already exists in /etc/pf.conf
+    if grep -q "$RULE" /etc/pf.conf; then
+        echo "$(date) | Port $PORT/$PROTO is already disabled."
+    else
+        echo "$(date) | Disabling port $PORT/$PROTO permanently..."
+
+        # Append the rule to /etc/pf.conf
+        echo "$RULE" | tee -a /etc/pf.conf > /dev/null
+
+        # Reload pf rules
+        pfctl -f /etc/pf.conf >/dev/null 2>&1
+        pfctl -E >/dev/null 2>&1
+
+        echo "$(date) | Port $PORT/$PROTO has been disabled permanently."
+    fi
+}
+
 # Start logging
 exec &> >(tee -a "$log")
 
@@ -184,16 +372,72 @@ else
     echo "$(date) | Skipping disabling Port Number 135 UDP..."
 fi
 
-# Disable Port Number 137-139 TCP
+# Disable Port Numbers 137-139 TCP
 if [ "$port137_139_tcp" = true ]; then
     port137_139_tcp
 else
     echo "$(date) | Skipping disabling Port Numbers 137-139 TCP..."
 fi
 
-# Disable Port Number 137-139 UDP
+# Disable Port Numbers 137-139 UDP
 if [ "$port137_139_udp" = true ]; then
     port137_139_udp
 else
-    echo "$(date) | Skipping disabling Port Number 137-139 UDP..."
+    echo "$(date) | Skipping disabling Port Numbers 137-139 UDP..."
+fi
+
+# Disable Port Number 445 TCP
+if [ "$port445_tcp" = true ]; then
+    port445_tcp
+else
+    echo "$(date) | Skipping disabling Port Number 445 TCP..."
+fi
+
+# Disable Port Numbers 1433-1434 TCP
+if [ "$port1433_1434_tcp" = true ]; then
+    port1433_1434_tcp
+else
+    echo "$(date) | Skipping disabling Port Numbers 1433-1434 TCP..."
+fi
+
+# Disable Port Numbers 1433-1434 UDP
+if [ "$port1433_1434_udp" = true ]; then
+    port1433_1434_udp
+else
+    echo "$(date) | Skipping disabling Port Numbers 1433-1434 UDP..."
+fi
+
+# Disable Port Number 3389 TCP
+if [ "$port3389_tcp" = true ]; then
+    port3389_tcp
+else
+    echo "$(date) | Skipping disabling Port Number 3389 TCP..."
+fi
+
+# Disable Port Number 1900 UDP
+if [ "$port1900_udp" = true ]; then
+    port1900_udp
+else
+    echo "$(date) | Skipping disabling Port Number 1900 UDP..."
+fi
+
+# Disable Port Numbers 20-21 TCP
+if [ "$port20_21_tcp" = true ]; then
+    port20_21_tcp
+else
+    echo "$(date) | Skipping disabling Port Numbers 120-21 TCP..."
+fi
+
+# Disable Port Numbers 20-21 UDP
+if [ "$port20_21_udp" = true ]; then
+    port20_21_udp
+else
+    echo "$(date) | Skipping disabling Port Numbers 20-21 UDP..."
+fi
+
+# Disable Port Number 23 TCP
+if [ "$port23_tcp" = true ]; then
+    port23_tcp
+else
+    echo "$(date) | Skipping disabling Port Number 23 TCP..."
 fi
